76c4b8a26611a7440b1f7cc767cc3be1527131d0
Jose Luis <joseluis060792@gmail.com>
2024-12-14 10:26:56 +0000

    U.4_estructura_de_datos.py

---
+++ U.4_estructura_de_datos.py
@@ -0,0 +1,25 @@
+# ESTRUCTURAS DE DATOS BÁSICAS
+
+print("1. LISTA")
+# Lista de colores
+colores = ["azul", "rojo", "verde"]
+print("Colores:", colores)
+print("Mi color favorito es:", colores[0])
+print("\n")
+
+print("2. TUPLA")
+# Tupla de meses
+meses = ("enero", "febrero", "marzo")
+print("Primer trimestre:", meses)
+print("Primer mes:", meses[0])
+print("\n")
+
+print("3. DICCIONARIO")
+# Diccionario de notas
+notas = {
+"matemáticas": 15,
+"historia": 17,
+"ciencias": 16
+}
+print("Mis notas:", notas)
+print("Nota de matemáticas:", notas["matemáticas"])
